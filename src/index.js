008fe14324cc7490e7b324a48e7ecf8f8f407c92
miss.daee <florchu161@gmail.com>
2021-10-02 18:31:27 -0300

    ejercicios terminados

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const ArrayCompras=["Pinolux", "Manteca", "Dulce de Leche"];
+const TituloPorDefecto= "Por favor inclui un titulo (titulo por defecto)";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const ListaDeCompras= ()=>{
+  return <ol>
+    <li>{ArrayCompras[0]}</li>
+    <li>{ArrayCompras[1]}</li>
+    <li>{ArrayCompras[2]}</li>
+  </ol>
+}
+
+const HolaMundo= (props)=>{
+
+const {titulo}= props;
+
+  if (props.titulo){ 
+  return (
+    <>
+     <h1>{titulo}</h1>
+    <ListaDeCompras />
+    </>
+  ) 
+}else{
+    return (
+      <>
+       <h1>{TituloPorDefecto}</h1>
+      <ListaDeCompras />
+      </>
+    )
+  }
+}
+
+const TituloFlecha= (props)=>{<h1>Bienvenido a  React (esto viene desde un componente)</h1>}
+
+
+const RootElement= document.getElementById("root")
+ReactDOM.render(<HolaMundo titulo= "Bienvenido a React (esto viene de una prop)"/>, <TituloFlecha />, RootElement)
+
+/*ejercicio 8*/
+/*Falta el import desde react.*/
+
+
+/*ejercicio  9 */
+/* a) no veria nada. */
+/* b) se rompe react*/
+/* c) I. Segun lo que entiendo esto es porque no estas pidiendo una parte del objeto escritor sino el nombre que le asigno a Autor */
+    /*II.deberia ddeclarar como constante a props y asignarle  el  valor nombre. Luego solicitar en return props*/
+/*d) En el html tengo que cambiar el id del ddiv por inicio tambien */
